d8124383db8ec9cba666150131a075ecc5ab7fbf
AchroDev <achrodev@proton.me>
2024-09-26 23:09:30 -0500

    Updated comment

--- main.cpp
+++ main.cpp
@@ -30,7 +30,8 @@ int main()
     // auto f = [x]() // Using 'x' by itself and not a reference, uses it as a value
     auto f = [=]() mutable // Writing the Lambda function this way allows us to declare everything as a mutable value in the body
     {
-        x++; // Now this works as the Lambda function has been declared mutable
+        x++; // Now this works as the Lambda function has been declared mutable, however it works the same as creating another variable as it still
+             // doesn't change the original value
         std::cout << x << std::endl;
     };
 
