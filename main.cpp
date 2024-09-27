e91d4e6af73874cdb67ffad20831ca9921d771e9
AchroDev <achrodev@proton.me>
2024-09-26 22:09:59 -0500

    Updated comment

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ class Entity
     // Single private member 'm_Name'
 private:
     std::string m_Name;
-    int m_DebugCount = 0;
+    int m_DebugCount = 0; // We don't wont to move this outside of the class as it is messy and makes it harder to track
     // Public section
 public:
     // Simple getter that gets and returns the name, we are enforcing const here so that we can use the
